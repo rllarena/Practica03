e06d790e97d28eb9bc4e3649328ae8e09c43fea6
Ruben Llarena <r_llarena@yahoo.com.mx>
2018-06-13 10:39:29 -0500

    ultima version, se agregan funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,25 +1,39 @@
 $(document).ready(function(){
-  $('#mensajeConfirmacion').hide();
-  $('#mensajeCanelacion').fadeOut();
+  $("#mensajeConfirmacion").hide();
+  $('#mensajeCancelacion').fadeOut();
 
   //evento click para el botón de guardar
-  $('#Guardar').click(function(){
+  $('#btn_Guardar').click(function(){
     //obtener valores del formulario
     var nombre=$('#txt_Nombre').val();
-    var apellidos=$('#txt_apellidos').val();
+    var apellidos=$("#txt_Apellidos").val();
     var sexo=$('#Lista_sexo').val();
+    var correo=$('#txt_correo').val();
+    var ocupacion=$('#txt_ocupacion').val();
+
+    //evento para ocultar los valores del formulario
+
+   /* $("#txt_Nombre").hide();
+    $("#txt_Apellidos").hide();
+    $("#txt_correo").hide();
+    $("#txt_ocupacion").hide();  */
+
 
     //asignar valores a etiquetas en el mensaje de confirmación
     $("#lbl_Nombre").text(nombre);
-    $("#lbl_Apellido").text(apellidos);
+    $("#lbl_Apellidos").text(apellidos);
     $("#lbl_Sexo").text(sexo);
+    $("#lbl_correo").text(correo);
+    $("#lbl_ocupacion").text(ocupacion);
 
-    $("mesnsajeConfirmacion").show();
+    $("#mensajeConfirmacion").show();
   });
 
   //evento click para el botón Cancelar
   $("#btn_Cancelar").click(function(){
-    $('#mesnsajeCanelacion').fadeIn();
+    $('#mensajeCancelacion').fadeIn();
+    $("#mensajeConfirmacion").hide();
+
   });
 
 });
